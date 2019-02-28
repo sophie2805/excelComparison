858ba1e0499b9f045d3584f01eb4f97b323be505
Sophie <sophie2805@163.com>
2019-02-28 17:19:12 +0800

    Add Junit test -- remove un-needed viarables

--- src/test/java/TestDifferenceEngine.java
+++ src/test/java/TestDifferenceEngine.java
@@ -15,37 +15,34 @@ public class TestDifferenceEngine {
     static String sourceFilePath = resourceDir.getAbsolutePath() + "/Source.xlsx";
     static String targetFilePath = resourceDir.getAbsolutePath() + "/Target.xlsx";
     static List<SheetData> source, target;
-    static List<String> comparisonResultSheet1, comparisonResultSheet2;
+    static List<String> comparisonResultSheet;
 
     @BeforeClass
     public static void ReadExcel() throws IOException{
         source = ExcelUtility.readExcel2007(sourceFilePath);
         target = ExcelUtility.readExcel2007(targetFilePath);
-        comparisonResultSheet1 = DifferenceEngine.Differ(source.get(0), target.get(0));
-        comparisonResultSheet2 = DifferenceEngine.Differ(source.get(1), target.get(1));
-        boolean a = comparisonResultSheet1.contains("Sheet 1");
-        int aa = comparisonResultSheet1.indexOf("Sheet 1");
+        comparisonResultSheet = DifferenceEngine.Differ(source.get(0), target.get(0));
     }
 
     @Test
     public void TestComparisonResultSheetName(){
-        Assert.assertTrue(comparisonResultSheet1.contains("Sheet Name: [Sheet 1]"));
+        Assert.assertTrue(comparisonResultSheet.contains("Sheet Name: [Sheet 1]"));
     }
 
     @Test
     public void TestComparisonResultCellDifference(){
-        Assert.assertTrue(comparisonResultSheet1.contains("Row 7"));
-        Assert.assertTrue(comparisonResultSheet1.contains("    Cell A7: source -- , target -- Title that not in source"));
+        Assert.assertTrue(comparisonResultSheet.contains("Row 7"));
+        Assert.assertTrue(comparisonResultSheet.contains("    Cell A7: source -- , target -- Title that not in source"));
     }
 
     @Test
     public void TestComparisonResultSummary(){
-        Assert.assertTrue(comparisonResultSheet1.contains("Discrepancy summary: Column A(title), Column C(album), Column D(isFound)"));
+        Assert.assertTrue(comparisonResultSheet.contains("Discrepancy summary: Column A(title), Column C(album), Column D(isFound)"));
     }
 
     @Test
     public void TestComparisonResultEmptyRow(){
-        Assert.assertTrue(comparisonResultSheet1.contains("Row 10"));
-        Assert.assertTrue(comparisonResultSheet1.contains("    Target row null..."));
+        Assert.assertTrue(comparisonResultSheet.contains("Row 10"));
+        Assert.assertTrue(comparisonResultSheet.contains("    Target row null..."));
     }
 }
